52864100401956570789caaac0644bd35209752c
victorhmrod <vhcgames1@gmail.com>
2026-05-13 18:14:54 -0300

    Revert "Add reCAPTCHA support to contact form"
    This reverts commit 6d258af9f99c86b7c30c66016acf7ac1118fff32.

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,6 @@
 // Use only your public Supabase URL and anon/publishable key here.
 // Never expose a service_role key in browser files.
 window.neoSoftSupabase = {
-  recaptchaSiteKey: '',
   url: '',
   anonKey: '',
   contentTable: 'site_content',
